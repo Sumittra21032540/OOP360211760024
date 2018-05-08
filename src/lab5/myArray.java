4a3c73e0d2aa7a28626089acc30ed4db9c10047f
Sumittra <Sumittra2103@gmail.com>
2018-05-08 16:17:16 +0700

    Input data arry

---
+++ src/lab5/myArray.java
@@ -0,0 +1,31 @@
+package lab5;
+
+import java.util.Scanner;
+
+public class myArray {
+    public static void main(String[] args) {
+
+        int a[] = new int[5];
+        System.out.println(a[0]);
+        a[0] = 10;
+        System.out.println(a[0]);
+        //input data to array
+
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < a.length; i++){
+            System.out.print("a["+i+"]:");
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Data in arry:");
+        for (int i = 0; i < a.length; i++){
+            System.out.print(a[i]+"\t");
+
+        }
+        System.out.println("\nEnhanced for Loop");
+
+        for (int val:a){
+            System.out.print(val+"\t");
+        }
+    }
+
+}
