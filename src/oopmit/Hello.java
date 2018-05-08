edbd9d0ac50fc5886e939fb971f01b2278639776
Sumittra <Sumittra2103@gmail.com>
2018-05-08 15:13:47 +0700

    update data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println ("Hello, My name is Sumittra");
-        
+        System.out.println ("Major: Information System");
+        System.out.println ("Faculty: Mangement Technology");
+        System.out.println("University: RMUTSV");
     }
 }
